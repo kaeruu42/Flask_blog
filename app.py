6c82956f8012fd9815a64733978c7ac282540f86
kaeruu42 <89044758+kaeruu42@users.noreply.github.com>
2022-09-26 22:13:27 -0400

    new post

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '{JWD0.-[sY4Y%r=|mdK"JjrkYR@gY'
+
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -19,6 +21,7 @@ def get_post(post_id):
         abort(404)
     return post
 
+
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -31,6 +34,26 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+        
+    return render_template('create.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
